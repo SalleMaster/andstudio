7008807356f9a83c3643b966c9c4381743f11db8
Sasa Radovanovic <salle90ar@gmail.com>
2020-05-01 15:08:58 +0200

    scroll down

--- App.js
+++ App.js
@@ -1,5 +1,27 @@
-const introSlide = document.querySelector('.intro-slide');
+// Slide In Intersection Observer
+const slideIn = document.querySelectorAll('.slide-in');
 
-// setTimeout(() => {
-//   introSlide.classList.toggle('animate');
-// }, 1000);
+const slideInOptions = {
+  // root: document.querySelector('#scrollArea'),
+  rootMargin: '0px',
+  threshold: 1.0,
+};
+
+const slideInCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('slide-in-animate');
+    }
+  });
+};
+
+const slideInObserver = new IntersectionObserver(
+  slideInCallback,
+  slideInOptions
+);
+
+slideIn.forEach((slide) => {
+  slideInObserver.observe(slide);
+});
