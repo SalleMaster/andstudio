be990eae5ac5f28e67b38554d3e373b9e4edd9dc
Sasa Radovanovic <salle90ar@gmail.com>
2020-05-02 22:58:26 +0200

    Gallery & Footer

--- App.js
+++ App.js
@@ -25,3 +25,31 @@ const slideInObserver = new IntersectionObserver(
 slideIn.forEach((slide) => {
   slideInObserver.observe(slide);
 });
+
+// Home Intro Observer
+// Change Nav Color To White
+const homeLatest = document.querySelector('#home-latest');
+const nav = document.querySelector('.nav');
+
+const homeLatestOptions = {
+  // root: document.querySelector('#home-works'),
+  rootMargin: '0px',
+  threshold: 0,
+};
+
+const homeLatestCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      nav.classList.add('nav-white');
+    } else {
+      nav.classList.remove('nav-white');
+    }
+  });
+};
+
+const homeLatestObserver = new IntersectionObserver(
+  homeLatestCallback,
+  homeLatestOptions
+);
+
+homeLatestObserver.observe(homeLatest);
