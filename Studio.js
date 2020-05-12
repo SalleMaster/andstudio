3a8352293db973beb6afbc111bfc3fdbd0413a21
Sasa Radovanovic <salle90ar@gmail.com>
2020-05-12 22:47:29 +0200

    Scroll Magic & GSAP animation

--- Studio.js
+++ Studio.js
@@ -54,3 +54,37 @@ const fadeInObserver = new IntersectionObserver(fadeInCallback, fadeInOptions);
 fadeIn.forEach((fade) => {
   fadeInObserver.observe(fade);
 });
+
+// Manifesto Animation
+scrollDescBox = document.querySelector('.scroll-desc-box');
+
+const scrollPath = {
+  curviness: 0,
+  autoRotate: false,
+  path: [
+    { x: 0, y: 0 },
+    { x: -scrollDescBox.scrollWidth + window.innerWidth, y: 0 },
+  ],
+};
+
+const tween = new TimelineLite();
+
+tween.add(
+  TweenLite.to('.scroll-desc-box', {
+    duration: 1,
+    motionPath: scrollPath,
+    ease: Power1.easeInOut,
+  })
+);
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: '.manifesto',
+  duration: 5000,
+  triggerHook: 0,
+})
+  .setTween(tween)
+  // .addIndicators()
+  .setPin('.manifesto')
+  .addTo(controller);
